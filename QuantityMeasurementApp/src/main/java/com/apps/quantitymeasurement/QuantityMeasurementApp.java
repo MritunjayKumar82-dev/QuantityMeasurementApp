bab2a52ecf0749ee6aa00641db6a17a57bee38a0
MritunjayKumar82-dev <mritunjaykumar30991@gmail.com>
2026-02-02 11:46:05 +0530

    added boolean method in main class

--- QuantityMeasurementApp/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,7 +2,15 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        System.out.println("Main Branch");
+        System.out.println(demonstrateLengthEquality( new Length(1.0,LengthUnit.FEET),new Length(12.0,LengthUnit.INCHES)));
+        System.out.println(demonstrateLengthEquality( new Length(1.0,LengthUnit.YARDS),new Length(36.0,LengthUnit.INCHES)));
+        System.out.println(demonstrateLengthEquality( new Length(1.0,LengthUnit.YARDS),new Length(3.0,LengthUnit.FEET)));
+        System.out.println(demonstrateLengthEquality( new Length(1.0,LengthUnit.CENTIMETERS),new Length(0.393701,LengthUnit.INCHES)));
+        System.out.println(demonstrateLengthEquality( new Length(2.0,LengthUnit.CENTIMETERS),new Length(2.0,LengthUnit.CENTIMETERS)));
+
+    }
+    public static boolean demonstrateLengthEquality(Length l1, Length l2){
+        return l1.equals(l2);
     }
 }
 
