09e917e0390fc4ce9493e787fd377a88d5d05ee6
MR20606901 <mr20606901@wipro.com>
2026-02-25 10:59:59 +0530

    Edited interface.

--- QuantityMeasurementApp/src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ QuantityMeasurementApp/src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -1,10 +1,8 @@
 package com.apps.quantitymeasurement;
 
 public interface IMeasurable {
-
     double getConversionFactor();
     default double convertToBaseUnit(double value){return value * getConversionFactor();}
     default double convertFromBaseUnit(double baseValue){return  baseValue/getConversionFactor();}
-
     String getUnitName();
 }
